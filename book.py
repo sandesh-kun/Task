ab6c2878323d7e5f7f95e3bba744eaa551df9b11
sandesh-kun <58624133+sandesh-kun@users.noreply.github.com>
2023-11-21 20:12:03 +0545

    Task2
    This is Guess Game.

--- book.py
+++ book.py
@@ -1,6 +1,26 @@
-#Task 1
+# #Task 1
 
-num =  int(input("Enter a number up to which for Even number: "))
+# num =  int(input("Enter a number up to which for Even number: "))
 
-for n in range(2, num + 1, 2):
-    print(n)
+# for n in range(2, num + 1, 2):
+#     print(n)
+    
+#Task 2
+import random
+guess_number = random.randint(1,100)
+att = 3
+
+for att in range (1, att+1, 1):
+     guess = int(input("Attempt {}:Enter guess number: ".format(att)))
+     
+     if guess == guess_number:
+         print("Correct Guess.")
+         break
+     else:
+         if guess < guess_number:
+             print("Higher than this number.")
+         else:
+             print("Lower than this number.")
+else:
+    print("The correct guess number was {}".format(guess_number))
+            
